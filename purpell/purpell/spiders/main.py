779f36fab9ea8816d1ab1c266acbe76e0996c50a
mayank <kalavadiyamayank72@gmail.com>
2024-12-26 07:58:13 +0530

    second commit

--- purpell/purpell/spiders/main.py
+++ purpell/purpell/spiders/main.py
@@ -1,16 +1,24 @@
 import scrapy
 import mysql.connector
 import requests
+from pathlib import Path
 from scrapy import selector, Selector
 
 
 class ProductDetailsSpider(scrapy.Spider):
     name = "product"
-    start_urls = ["https://www.purplle.com/"]
+    domain = ["https://www.purplle.com/"]
+    start_url=["https://www.purplle.com/page/1/",
+               "https://www.purplle.com/page/2/"]
 
     def __init__(self):
         print("ok")
 
+    # def parse(self, response):
+    #     page = response.url.split("/")[-2]
+    #     filename = f"product-{page}.html"
+    #     Path(filename).write_bytes(response.body)
+    #     self.log(f"Saved file {filename}")
 
 p=ProductDetailsSpider()
 
@@ -34,7 +42,13 @@ params = {
 response = requests.get('https://www.purplle.com/search', params=params, headers=headers)
 print(response)
 select=Selector(text=response.text)
-text=select.xpath('.//div[@class="product-title fs-7 text-start text-black fw-normal"]/text()').get()
-title=select.xpath('.//span[@class="fw-bold float-start position-relative clearfix w-100 text-center ng-star-inserted"]/text()').get()
-print(text)
-print(title)
+name=select.xpath('//div[@class="product-title fs-7 text-start text-black fw-normal"]/text()').get()
+price=select.xpath('//span[@class="text-black fw-bolder fs-6 f18i"]/text()').get()
+defordicount=select.xpath('//span[@class="fw-bold float-start position-relative clearfix w-100 text-center ng-star-inserted"]/text()').get()
+rateing=select.xpath('//span[@class="star-rating bg-success rounded-4 fs-8 text-white fw-bold ng-star-inserted"]/text()').get()
+userreview=select.xpath('text-black-50 ms-1 fs-8 ng-star-inserted').get()
+
+
+print(price)
+print(name)
+# p.parse(response)
